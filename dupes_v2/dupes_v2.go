b091cc71a3392d11ae66ee626e7885c96e92702c
Dmitry Khaleev <dkhaleev@gmail.com>
2019-01-30 00:11:20 +0200

    homework 1.4: count, string and name of the origin file

--- dupes_v2/dupes_v2.go
+++ dupes_v2/dupes_v2.go
@@ -13,9 +13,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	references := make(map[string]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, references)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -23,22 +24,23 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dupes encountered error: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, references)
 			f.Close()
 		}
 	}
 
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t\t\t%s\n", n, line, references[line])
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, references map[string]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		references[input.Text()] = f.Name()
 	}
 	//ignore potential errors from input.Err()
 }
