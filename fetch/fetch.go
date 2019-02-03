27e2f74c8a7e9e6238655b606a5d74a37031e4ee
Dmitry Khaleev <dkhaleev@gmail.com>
2019-02-03 22:21:33 +0200

    simple URL fecher

---
+++ fetch/fetch.go
@@ -0,0 +1,32 @@
+/**
+Fecth given URL
+Example from the Donovan and Kernighan book
+*/
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		body, err := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", body)
+	}
+}
