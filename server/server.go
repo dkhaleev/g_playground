a736ab7637ebf8fdd7fb5d0f7f12dbbf663c5ea8
Dmitry Khaleev <dkhaleev@gmail.com>
2019-02-07 23:56:45 +0200

    bare-minimal web server

---
+++ server/server.go
@@ -0,0 +1,20 @@
+/**
+bare minimal web-server
+Example from the Donovan and Kernighan book
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+}
