c57d0d0d289278975e7ce274427d198493d62425
Dmitry Khaleev <dkhaleev@gmail.com>
2019-02-21 22:35:01 +0200

    ex. 2.1: FtC and CtF CLI converter

--- FtoC/ftoc.go
+++ FtoC/ftoc.go
@@ -7,16 +7,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
 	const freezingF, boilingF, burningF = 32.0, 212.0, 451.0
-	fmt.Printf("Boiling temperature of water %g Fahrenheit and %g Celsius \n", boilingF, fToC(boilingF))
-	fmt.Printf("Freezing temperature of water %g Fahreheit and %g Celsius \n", freezingF, fToC(freezingF))
-	fmt.Printf("Burning temerature of paper %g Fahrenheit and %g Celsius \n", burningF, fToC(burningF))
+
+	if len(os.Args) < 3 {
+		fmt.Println("Too few arguments. Temp or Scale missing")
+		os.Exit(1)
+	}
+
+	scale := os.Args[2]
+
+	temp, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		log.Print(err)
+	}
+
+	switch scale {
+	case "F":
+		fmt.Println("FtC conversion")
+		fmt.Printf("%g degrees Fahrenheit equals %g Celsius\n", temp, ftoC(temp))
+		break
+	case "C":
+		fmt.Println("CtF conversion")
+		fmt.Printf("%g degrees Celsius equals %g Fahrenheit\n", temp, ctoF(temp))
+		break
+	default:
+		fmt.Println("Unknown scale")
+		os.Exit(1)
+	}
 }
 
 //function converts fahrenheit degrees to celsius
-func fToC(f float64) float64 {
+func ftoC(f float64) float64 {
 	return (f - 32) * 5 / 9
+}
+
+//function converts celsuis to fahrenheit
+func ctoF(c float64) float64 {
+	return (c * 9 / 5) + 32
 }
