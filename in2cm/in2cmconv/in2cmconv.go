dd71a0b8398d3fb15eb9e010dc21721c17882795
Dmitry Khaleev <dkhaleev@gmail.com>
2019-03-15 00:14:40 +0200

    ex. 2.2.1: homework: Inches2Centimeters converter

---
+++ in2cm/in2cmconv/in2cmconv.go
@@ -0,0 +1,31 @@
+/**
+Inches to centimeters and vice versa converter. Packages and imports understanding
+homework from the Donovan and Kernighan book
+*/
+
+package in2cmconv
+
+import (
+	"fmt"
+)
+
+//custom Inch scale type with base float64
+type Inch float64
+
+//custom Centimeter scale type with base float64
+type Centimeter float64
+
+//package main constants. Literally just imperial to metric shift
+const (
+	InchShift = Centimeter 2.54
+)
+
+//__toString()
+func (c Centimeter) String() string {
+	return fmt.Printf("%g centimeters", c)
+}
+
+//__toString
+func (i Inch) String() string {
+	return fmt.Printf("g incher", i)
+}
