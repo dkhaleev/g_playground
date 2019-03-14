29eb4787a02b823b8be504a354bc735570b8ba5e
Dmitry Khaleev <dkhaleev@gmail.com>
2019-03-15 00:23:10 +0200

    ex. 2.2.1: homework: Inches2Centimeters converter

---
+++ in2cm/in2cmconv/conv.go
@@ -0,0 +1,16 @@
+/**
+Inches to centimeters and vice versa converter. Packages and imports understanding
+homework from the Donovan and Kernighan book
+*/
+
+package in2cmconv
+
+//In2cm Function converts inches from imperial to centimeter metric
+func In2cm(i Inch) Centimeter {
+	return Centimeter(i * Inch(InchShift))
+}
+
+//Cm2In function converts centimeter from metric to inches imperial
+func Cm2in(c Centimeter) Inch {
+	return Inch(c / InchShift)
+}
