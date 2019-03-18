843a74b4200970fcd413306881aa4796e4c4b0f8
Dmitry Khaleev <dkhaleev@gmail.com>
2019-03-18 21:59:56 +0200

    ex.2.3.1: Understanding init()

---
+++ popcount/counter.go
@@ -0,0 +1,27 @@
+/**
+count of all set bits in byte. Understanding init()
+Example from the Donovan and Kernighan book
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"./popcount"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		i, err := strconv.ParseUint(arg, 10, 32)
+		if err != nil {
+			fmt.Println("Argument parse error")
+			os.Exit(1)
+		}
+
+		fmt.Printf("set bits total in byte %d\n", popcount.Popcount(i))
+
+	}
+}
