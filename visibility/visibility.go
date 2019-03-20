4a2943c10c8d98827de47c22533165d4223ce515
Dmitry Khaleev <dkhaleev@gmail.com>
2019-03-20 23:39:28 +0200

    ex. 2.7.2: variables visibility

--- visibility/visibility.go
+++ visibility/visibility.go
@@ -5,7 +5,22 @@ Example from the Donovan and Kernighan book
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var cwd string
+
+func init() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error: Getwd: %v", err)
+	}
+
+	log.Printf("Current working dir: %s", cwd)
+}
 
 func main() {
 	x := "hello!"
