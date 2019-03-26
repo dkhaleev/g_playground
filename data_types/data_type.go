5b63eb6e3852d819ef925f8f04c928b377f3a6f2
Dmitry Khaleev <dkhaleev@gmail.com>
2019-03-26 21:46:28 +0200

    ex. 3.0.4: floats

--- data_types/data_type.go
+++ data_types/data_type.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 var a uint8 = 1<<1 | 1<<5
@@ -15,6 +16,7 @@ var metals = []string{"silver", "copper", "steel", "brass", "zinc"}
 func main() {
 	o := 0666
 	var p = int64(0xdeadbeef)
+
 	fmt.Printf("%08b\n", a)
 	fmt.Printf("%08b\n", b)
 	fmt.Printf("%08b\n", a&b)
@@ -34,4 +36,8 @@ func main() {
 
 	fmt.Printf("%d %[1]o %#[1]o\n", o)
 	fmt.Printf("%d %[1]o %#[1]o %#[1]x %#[1]X %08[1]b\n", p)
+
+	for j := 0; j < 8; j++ {
+		fmt.Printf("j = %d, e^j = %8.3f\n", j, math.Exp(float64(j)))
+	}
 }
