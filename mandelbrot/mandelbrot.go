e17a15dcfe62321d9df395f1fd71120cd468d45c
Dmitry Khaleev <dkhaleev@gmail.com>
2019-07-03 23:03:16 +0300

    ex.3.6.homework: supersampling

--- mandelbrot/mandelbrot.go
+++ mandelbrot/mandelbrot.go
@@ -13,20 +13,50 @@ import (
 	"os"
 )
 
+const (
+	width  = 2048
+	height = 2048
+)
+
+var imageData [width][height]color.Color
+var mask [width / 2][height / 2]color.Color
+
 func main() {
+	// fill the image array
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height          = 1024, 1024
 	)
 
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
 
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+			imageData[px][py] = mandelbrot(z)
+		}
+	}
+	//fill the sampled mask
+	for sy := 0; sy < height/2; sy++ {
+		for sx := 0; sx < width/2; sx++ {
+			//quarterize
+			ra, ga, ba, _ := color.Color.RGBA(imageData[sx*2][sy*2])
+			rb, gb, bb, _ := color.Color.RGBA(imageData[sx*2+1][sy*2])
+			rc, gc, bc, _ := color.Color.RGBA(imageData[sx*2][sy*2+1])
+			rd, gd, bd, _ := color.Color.RGBA(imageData[sx*2+1][sy*2+1])
+			//equalize
+			r := (ra + rb + rc + rd) / 4
+			g := (ga + gb + gc + gd) / 4
+			b := (ba + bb + bc + bd) / 4
+			//fill the mask
+			mask[sx][sy] = color.RGBA{uint8(r), uint8(g), uint8(b), 255}
+		}
+	}
+	//render image
+	img := image.NewRGBA(image.Rect(0, 0, width/2, height/2))
+	for ix := 0; ix < height/2; ix++ {
+		for iy := 0; iy < width/2; iy++ {
+			img.Set(ix, iy, mask[ix][iy])
 		}
 	}
 	png.Encode(os.Stdout, img)
@@ -45,10 +75,6 @@ func mandelbrot(z complex128) color.Color {
 	}
 	return color.Black
 }
-
-// Map the number of iterations into the color pallet
-//
-// c.f. http://www.rapidtables.com/convert/color/hsv-to-rgb.htm
 
 func map2RGB(n uint8) color.Color {
 
